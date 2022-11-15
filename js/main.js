ea2b249664d88a8203421fe238e5868c5dd19c43
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-11-15 15:08:59 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,41 @@
 
-var a = 0;
-var b = 0;
+var aStr = "";
+var bStr = "";
 var operator = "";
+var switchVar = false;
 
 function addNumber(num){
-  console.log(num);
+  if (!switchVar){
+    aStr += num;
+  }else{
+    bStr += num;
+  }
 }
+
+function addOp(op){
+  operator = op;
+  switchVar = true;
+}
+
+function calc(){
+  var result = doCalc();
+  console.log(result);
+  document.getElementById('out').innerHTML = result;
+}
+
+function doCalc(){
+  switch (operator) {
+    case "+":
+      return parseInt(aStr) + parseInt(bStr);
+    case "-":
+      return parseInt(aStr) - parseInt(bStr);
+    case "/":
+      return parseInt(aStr) / parseInt(bStr);
+    case "x":
+      return parseInt(aStr) * parseInt(bStr);
+  }
+}
+
 
 function clear(){
   a = 0;
