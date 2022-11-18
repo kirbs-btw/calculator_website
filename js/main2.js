f62a253847b8def33c034596832df5fd36dccb2e
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-11-18 22:07:26 +0100

    Add files via upload

---
+++ js/main2.js
@@ -0,0 +1,17 @@
+var userInput = "";
+
+function resetInput(){
+  calc = "";
+}
+
+function addElement(e){
+  userInput += e;
+}
+
+function compileInput(){
+  for (var i = 0; i < userInput.length; i++) {
+    
+  }
+
+
+}
